983af03645ef3fecce47b76b0b50a6dd76fd545d
aashish <orry.14here@gmail.com>
2025-03-09 10:13:52 +0530

    deletef from aashidsh

--- Waste-Management/backend/test.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-try:
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["smartdustbin"]
-    print("✅ MongoDB Connected Successfully!")
-except Exception as e:
-    print("❌ MongoDB Connection Failed:", e)
